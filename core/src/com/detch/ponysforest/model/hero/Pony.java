36baf0e4667f421cf74aea1d40d64a7212815f9b
detch <lesha99703@gmail.com>
2019-09-28 02:34:43 +0300

    Started Box2d integration

--- core/src/com/detch/ponysforest/model/hero/Pony.java
+++ core/src/com/detch/ponysforest/model/hero/Pony.java
@@ -2,15 +2,50 @@ package com.detch.ponysforest.model.hero;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.detch.ponysforest.service.AssetsManager;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class Pony {
 
     private Sprite sprite;
+    private Body body;
+    private Vector2 position;
+    private World world;
 
-    public Pony() {
+    public Pony(float x, float y, World world) {
         sprite = new Sprite();
+        position = new Vector2(x, y);
+        this.world = world;
+
         sprite.setRegion(AssetsManager.getPonyTexture());
-        sprite.setPosition(0, 0);
+        sprite.setPosition(position.x, position.y);
+
+        createBody();
+    }
+
+    private void createBody() {
+        BodyDef bodyDef = new BodyDef();
+        FixtureDef fixtureDef = new FixtureDef();
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(2,2);
+
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(position);
+        body = world.createBody(bodyDef);
+
+        fixtureDef.shape = shape;
+        fixtureDef.density = 5;
+        fixtureDef.restitution = 1f;
+        body.createFixture(fixtureDef);
+        body.setUserData(sprite);
+    }
+
+    public Body getBody() {
+        return body;
     }
 
     public Sprite getSprite() {
--- core/src/com/detch/ponysforest/view/ForestLevel.java
+++ core/src/com/detch/ponysforest/view/ForestLevel.java
@@ -1,10 +1,13 @@
 package com.detch.ponysforest.view;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.detch.ponysforest.model.hero.Pony;
@@ -15,6 +18,7 @@ public class ForestLevel implements Screen {
     private Texture background;
     private SpriteBatch batch;
     private OrthographicCamera camera;
+    private Box2DDebugRenderer renderer;
     private World world;
     private Stage stage;
     private Pony pony;
@@ -22,20 +26,26 @@ public class ForestLevel implements Screen {
     @Override
     public void show() {
         background = AssetsManager.getForestTexture();
-        pony = new Pony();
-        camera = new OrthographicCamera();
-        world = new World(new Vector2(20, 20), true);
+        world = new World(new Vector2(0, -9.8f), true);
+        pony = new Pony(0, 15, world);
+        camera = new OrthographicCamera(20,15);
+        camera.position.set(10, 7.5f, 0);
+        renderer = new Box2DDebugRenderer();
         stage = new Stage();
         batch = new SpriteBatch();
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         camera.update();
+        world.step(1/60f, 4, 4);
         batch.begin();
         batch.draw(background, 0, 0);
-        batch.draw(pony.getSprite(), pony.getSprite().getX(), pony.getSprite().getY());
+        batch.draw(pony.getSprite(), pony.getBody().getPosition().x, pony.getBody().getPosition().y);
         batch.end();
+        renderer.render(world, camera.combined);
+
     }
 
     @Override
